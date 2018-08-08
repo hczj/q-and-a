76b52b2493b28d23568d1294461eeae564d0dd34
Chris Lusk <chris.m.lusk@gmail.com>
2018-08-08 12:02:05 -0500

    add user and question models

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -3,10 +3,25 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 
 const User = db.define('user', {
+  firstName: {
+    type: Sequelize.STRING,
+    // allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    // allowNull: false
+  },
   email: {
     type: Sequelize.STRING,
     unique: true,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  isAdmin: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
   },
   password: {
     type: Sequelize.STRING,
