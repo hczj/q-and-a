7f9e3358605e8e106fcb499a02df3b41c9d5ef45
Chris Lusk <chris.m.lusk@gmail.com>
2018-08-09 09:42:36 -0500

    refactor auth, start manage view

--- client/store/users.spec.js
+++ client/store/users.spec.js
@@ -1,7 +1,7 @@
 /* global describe beforeEach afterEach it */
 
 import { expect } from 'chai';
-import { me, logout } from './user';
+import { me, logout } from './users';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import configureMockStore from 'redux-mock-store';
@@ -28,23 +28,24 @@ describe('thunk creators', () => {
   });
 
   describe('me', () => {
-    it('eventually dispatches the GET USER action', async () => {
+    it('eventually dispatches the RECEIVE_ME action', async () => {
       const fakeUser = { email: 'Cody' };
       mockAxios.onGet('/auth/me').replyOnce(200, fakeUser);
       await store.dispatch(me());
       const actions = store.getActions();
-      expect(actions[0].type).to.be.equal('GET_USER');
+      expect(actions[0].type).to.be.equal('REQUEST_ME');
+      expect(actions[1].type).to.be.equal('RECEIVE_ME');
       expect(actions[0].user).to.be.deep.equal(fakeUser);
     });
   });
 
   describe('logout', () => {
-    it('logout: eventually dispatches the REMOVE_USER action', async () => {
+    it('logout: eventually dispatches the REMOVE_ME action', async () => {
       mockAxios.onPost('/auth/logout').replyOnce(204);
       await store.dispatch(logout());
       const actions = store.getActions();
-      expect(actions[0].type).to.be.equal('REMOVE_USER');
-      expect(history.location.pathname).to.be.equal('/login');
+      expect(actions[0].type).to.be.equal('REMOVE_ME');
+      expect(history.location.pathname).to.be.equal('/');
     });
   });
 });
--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../db/models');
+const { isAdmin } = require('../utils')
+
 module.exports = router;
 
 router.get('/', async (req, res, next) => {
@@ -8,9 +10,50 @@ router.get('/', async (req, res, next) => {
       // explicitly select only the id and email fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'email']
+      // attributes: ['id', 'email']
     });
     res.json(users);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put('/:userId', isAdmin, async (req, res, next) => {
+  try {
+    const { data: user } = await User.update(
+      {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        isAdmin: req.body.isAdmin,
+        password: req.body.password
+      },
+      {
+        where: { id: req.params.userId },
+        returning: true,
+        plain: true
+      }
+    );
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/:userId', isAdmin, async (req, res, next) => {
+  const userId = +req.params.userId;
+  try {
+    await User.destroy({ where: { id: userId } });
+    res.json(userId);
   } catch (err) {
     next(err);
   }
