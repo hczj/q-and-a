fc1c64bf33b90b1c68d8b7b07c2fcc7059cef9c1
Chris Lusk <chris.m.lusk@gmail.com>
2018-08-09 12:12:11 -0500

    hook up oauth

--- server/auth/google.js
+++ server/auth/google.js
@@ -4,20 +4,6 @@ const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const { User } = require('../db/models');
 module.exports = router;
 
-/**
- * For OAuth keys and other secrets, your Node process will search
- * process.env to find environment variables. On your production server,
- * you will be able to set these environment variables with the appropriate
- * values. In development, a good practice is to keep a separate file with
- * these secrets that you only share with your team - it should NOT be tracked
- * by git! In this case, you may use a file called `secrets.js`, which will
- * set these environment variables like so:
- *
- * process.env.GOOGLE_CLIENT_ID = 'your google client id'
- * process.env.GOOGLE_CLIENT_SECRET = 'your google client secret'
- * process.env.GOOGLE_CALLBACK = '/your/google/callback'
- */
-
 if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
   console.log('Google client ID / secret not found. Skipping Google OAuth.');
 } else {
@@ -30,13 +16,15 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
   const strategy = new GoogleStrategy(
     googleConfig,
     (token, refreshToken, profile, done) => {
+      // console.log('profile', profile)
       const googleId = profile.id;
-      const name = profile.displayName;
+      const firstName = profile.name.givenName;
+      const lastName = profile.name.familyName;
       const email = profile.emails[0].value;
 
       User.findOrCreate({
         where: { googleId },
-        defaults: { name, email }
+        defaults: { firstName, lastName, email }
       })
         .then(([user]) => done(null, user))
         .catch(done);
@@ -50,7 +38,7 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
   router.get(
     '/callback',
     passport.authenticate('google', {
-      successRedirect: '/home',
+      successRedirect: '/',
       failureRedirect: '/login'
     })
   );
