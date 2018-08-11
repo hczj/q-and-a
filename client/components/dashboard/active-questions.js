bef0ee8a75b68a3cd7502dd427d340e89d964789
Hollie Lambert <17777636+hollielu@users.noreply.github.com>
2018-08-11 16:23:11 -0500

    add func to filter active questions

--- client/components/dashboard/active-questions.js
+++ client/components/dashboard/active-questions.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Header, QuestionCard } from '../../components';
 
-const ActiveQuestions = ({ questions }) => {
+const ActiveQuestions = ({ isLoading, questions, myId }) => {
+  const activeQs = questions.filter(
+    question => question.userId === myId && question.isActive
+  );
+
+  if (isLoading || activeQs.length === 0) return null;
   return (
     <div className="box">
       <Header title="Unanswered Questions" />
-
       <div className="columns">
-        {questions.map(question => (
+        {activeQs.map(question => (
           <QuestionCard key={question.id} {...question} />
         ))}
       </div>
@@ -17,7 +21,9 @@ const ActiveQuestions = ({ questions }) => {
 };
 
 const mapState = state => ({
-  questions: state.questions.all
+  myId: state.me.id,
+  questions: state.questions.all,
+  isLoading: state.questions.isLoading
 });
 
 export default connect(mapState)(ActiveQuestions);
