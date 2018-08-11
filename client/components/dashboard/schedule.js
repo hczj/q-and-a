4e45677f1836734d643fc24036910dd37ff9634b
Hollie Lambert <17777636+hollielu@users.noreply.github.com>
2018-08-11 16:27:48 -0500

    simple schedule structure

---
+++ client/components/dashboard/schedule.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Header } from '../../components';
+
+const Schedule = () => {
+  return (
+    <div className="box">
+      <Header title="Schedule" />
+    </div>
+  );
+};
+
+export default Schedule;
