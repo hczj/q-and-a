4a3a7678bb1b5b22cbd33bec05394e240cf5c4e4
Chris Lusk <chris.m.lusk@gmail.com>
2018-08-16 10:15:55 -0500

    Update messageList.js

--- client/components/inbox/messageList.js
+++ client/components/inbox/messageList.js
@@ -1,34 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const MessageList = ({ messages, myId }) => {
-  //   console.log('isLoadingActive', isLoadingActive);
-  //   console.log('thread', thread);
+const MessageList = ({ messages, myId }) => (
+  <div className="box">
+    {messages &&
+      messages.map(message => (
+        <article
+          key={message.id}
+          className={`message is-${message.userId === myId ? '-primary' : '-dark'}`}
+        >
+          <div className="message-body">{message.content}</div>
+        </article>
+      ))}
+  </div>
+);
 
-  return (
-    <div className="box">
-      {messages &&
-        messages.map(message => {
-          return (
-            <div key={message.id}>
-              <article
-                className={`message ${
-                  message.userId === myId ? 'is-primary' : 'is-dark'
-                }`}
-              >
-                <div className="message-body">{message.content}</div>
-              </article>
-            </div>
-          );
-        })}
-    </div>
-  );
-};
-
-// const mapState = state => ({
-//   isLoadingActive: state.threads.isLoadingActive,
-//   thread: state.threads.active
-// });
-
-// export default connect(mapState)(MessageList);
 export default MessageList;
