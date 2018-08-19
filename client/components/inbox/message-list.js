6ef55af6bc75f1cf8ea4747152770fc63b07ad31
Chris Lusk <chris.m.lusk@gmail.com>
2018-08-19 10:45:14 -0500

    add linkify util so messages can contain links

--- client/components/inbox/message-list.js
+++ client/components/inbox/message-list.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { linkify } from '../../utils';
 
 const MessageList = ({ thread, myId }) => (
   <div className="thread-message-list">
@@ -11,7 +12,10 @@ const MessageList = ({ thread, myId }) => (
             message.userId === myId ? 'primary' : 'secondary'
           }`}
         >
-          <div className="message-body">{message.content}</div>
+          <div
+            className="message-body"
+            dangerouslySetInnerHTML={{ __html: linkify(message.content) }}
+          />
         </div>
       ))}
   </div>
--- client/utils.js
+++ client/utils.js
@@ -6,20 +6,30 @@ export const getRoomId = (userId = 0) => {
   return userId + Math.round(Math.random() * 36 ** 12).toString(36);
 };
 
+export const linkify = text => {
+  const urls = /\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]/gim; // `http://`, `https://`, `ftp://`
+  const pseudoUrls = /(^|[^\/])(www\.[\S]+(\b|$))/gim; // `www.` by itself
+  const emails = /[\w.]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+/gim;
+
+  return text
+    .replace(urls, '<a href="$&">$&</a>')
+    .replace(pseudoUrls, '$1<a href="https://$2">$2</a>')
+    .replace(emails, '<a href="mailto:$&">$&</a>');
+};
+
 export const slugify = text => {
-  const a = `àáäâèéëêìíïîòóöôùúüûñ`
-  const b = `aaaaeeeeiiiioooouuuun`
-  const p = new RegExp(a.split('').join('|'), 'g')
+  const a = `àáäâèéëêìíïîòóöôùúüûñ`;
+  const b = `aaaaeeeeiiiioooouuuun`;
+  const p = new RegExp(a.split('').join('|'), 'g');
 
   return text
     .toString()
     .toLowerCase()
-    .replace(/<[^>]+>/gi, '')   // replace HTML tags
-    .replace(p, c =>
-        b.charAt(a.indexOf(c))) // replace special characters
-    .replace(/&/g, 'and')       // replace & with 'and'
+    .replace(/<[^>]+>/gi, '') // replace HTML tags
+    .replace(p, c => b.charAt(a.indexOf(c))) // replace special characters
+    .replace(/&/g, 'and') // replace & with 'and'
     .replace(/[^\w\s-]+/g, '-') // replace non-word chars, spaces with dash
-    .replace(/ +/g, '-')        // replace multiple spaces with dash
-    .replace(/-{2,}/g, '-')     // replace multiple dashes with single dash
-    .replace(/^-+|-+$/g, '');   // remove leading and trailing dash
-}
+    .replace(/ +/g, '-') // replace multiple spaces with dash
+    .replace(/-{2,}/g, '-') // replace multiple dashes with single dash
+    .replace(/^-+|-+$/g, ''); // remove leading and trailing dash
+};
