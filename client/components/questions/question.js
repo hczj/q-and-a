3695c420048bcc07fbce69de538cc3c71e0009a9
Chris Lusk <chris.m.lusk@gmail.com>
2018-08-21 14:18:58 -0500

    redesign question cards

--- client/components/questions/question.js
+++ client/components/questions/question.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
 import { Button } from '../../components';
@@ -6,62 +6,61 @@ import { Button } from '../../components';
 const Question = ({ question, isTeacher, closeQuestion, myId }) => {
   const { title, description, topics, user, createdAt } = question;
   return (
-    <div className="box">
+    <div className="question-card">
       <div className="columns">
-        <div className="column is-one-fifth">
-          <figure className="image is-96x96">
-            <img className="is-rounded" src={user.imageUrl} />
-          </figure>
-          <Link to={`/profile/${user.id}`}>{`${user.firstName} ${
-            user.lastName
-          }`}</Link>
-          <div>asked {moment(createdAt).fromNow()}</div>
+        <div className="column question-student">
+          <Link to={`/profile/${user.id}`}>
+            <figure className="image is-64x64">
+              <img className="is-rounded" src={user.imageUrl} />
+            </figure>
+            <p className="name">{user.name}</p>
+          </Link>
+          <div className="time">asked {moment(createdAt).fromNow()}</div>
         </div>
-
-        <div className="column">
-          <nav className="level">
-            <div className="level-left">
-              <div className="level-item">
-                <Link to={`/questions/question/${question.id}`}>
-                  <h1 className="title is-4">{title}</h1>
-                </Link>
-              </div>
+        <div className="column question-content">
+          <div className="content">
+            <h2 className="title is-4">
+              <Link to={`/questions/question/${question.id}`}>
+                {title}
+              </Link>
+            </h2>
+            <p>{description}</p>
+            <div className="tags">
+              {topics.map(topic => (
+                <span key={topic.id} className="tag is-rounded">
+                  {topic.name}
+                </span>
+              ))}
             </div>
-
-            <div className="level-right">
-              <div className="level-item">
-                {isTeacher && (
-                  <Button
-                    link="/classroom"
-                    text="Answer"
-                    classes="button is-link"
-                    state={{
-                      questionId: question.id,
-                      studentId: user.id,
-                      teacherId: myId
-                    }}
-                  />
-                )}
-                {myId === question.userId && (
-                  <button
-                    type="button"
-                    className="button is-link"
-                    onClick={() => closeQuestion(question)}
-                  >
-                    Remove
-                  </button>
-                )}
-              </div>
-            </div>
-          </nav>
-          <div className="content">{description}</div>
-          <div className="tags">
-            {topics.map(topic => (
-              <span key={topic.id} className="tag">
-                {topic.name}
-              </span>
-            ))}
           </div>
+        </div>
+        <div className="column question-actions">
+          {isTeacher && (
+            <Button
+              link="/classroom"
+              text="Answer"
+              classes="button is-link"
+              state={{
+                questionId: question.id,
+                studentId: user.id,
+                teacherId: myId
+              }}
+            />
+          )}
+          {myId === question.userId && (
+            <button
+              type="button"
+              className="button is-link"
+              onClick={() => closeQuestion(question)}
+            >
+              Remove
+            </button>
+          )}
+          <Button
+            link={`/questions/question/${question.id}`}
+            text="See more"
+            classes="button is-outlined is-link"
+          />
         </div>
       </div>
     </div>
