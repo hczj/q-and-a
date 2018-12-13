6a0ee3bc9fdc9555ce6103510f5aca72af4d7a4d
Chris Lusk <chris.m.lusk@gmail.com>
2018-12-13 08:16:14 -0600

    refactor from class to func

--- client/components/auth/auth.js
+++ client/components/auth/auth.js
@@ -1,25 +1,16 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Login, Signup } from '../../components';
 
-class Auth extends Component {
-  render() {
-    return (
-      <div className="columns">
-        <div className="column is-6 is-4-desktop is-offset-3 is-offset-4-desktop">
-          <Switch>
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={Signup} />
-          </Switch>
-        </div>
-      </div>
-    );
-  }
-}
+const Auth = () => (
+  <div className="columns">
+    <div className="column is-6 is-4-desktop is-offset-3 is-offset-4-desktop">
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+      </Switch>
+    </div>
+  </div>
+);
 
-const mapState = state => ({
-  isLoggedIn: !!state.me.isLoggedIn
-});
-
-export default connect(mapState)(Auth);
+export default Auth;
